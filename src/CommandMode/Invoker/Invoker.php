c1d735a28336c08b03397615fc351cc6b71cf746
雪狼 <zhaijunliang@vchangyi.com>
2022-07-22 17:56:03 +0800

    命令模式优化

--- src/CommandMode/Invoker/Invoker.php
+++ src/CommandMode/Invoker/Invoker.php
@@ -3,12 +3,13 @@
 
 namespace Test\CommandMode\Invoker;
 
+use Test\CommandMode\InterfaceBase\Command;
 
 class Invoker
 {
     private $_command;
 
-    public function __construct($command)
+    public function __construct(Command $command)
     {
         $this->_command = $command;
     }
